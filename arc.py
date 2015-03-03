b76b42d4fd664130e1cb2f5cdaf7f0e947bdcdea
Alex Grede <agrede@gmail.com>
2015-03-03 14:01:13 -0500

    Added wavelength range check

--- arc.py
+++ arc.py
@@ -1,5 +1,6 @@
 from ctypes import *
 import numpy as np
+from pyvd_common import within_limits
 
 
 class SpecPro():
@@ -7,10 +8,12 @@ class SpecPro():
     Uses ARC_SpectraPro.dll to interface with mono
     """
     _mono = c_int(32)
+    wavelength_limits = [0, 20000]
 
     def __init__(self, com_port, dll_path="C:\\Program Files (x86)\\Princeton Instruments\\MonoControl\\ARC_SpectraPro.dll"):
         self._dll = windll.LoadLibrary(dll_path)
         self._dll.ARC_Open_Mono_Port(com_port, byref(self._mono))
+        self.get_limits()
 
     def close(self):
         self._dll.ARC_Close_Mono(self._mono)
@@ -23,4 +26,12 @@ class SpecPro():
 
     @wavelength.setter
     def wavelength(self, value):
-        self._dll.ARC_set_Mono_Wavelength_nm(self._mono, c_double(value))
+        if (within_limits(value, self.wavelength_limits)):
+            self._dll.ARC_set_Mono_Wavelength_nm(self._mono, c_double(value))
+
+    def get_limits(self):
+        tmp = c_double()
+        self._dll.ARC_get_Mono_Wavelength_Cutoff_nm(self._mono, byref(tmp))
+        self.wavelength_limits[1] = tmp
+        self._dll.ARC_get_Mono_Wavelength_Min_nm(self._mono, byref(tmp))
+        self.wavelength_limits[0] = tmp
