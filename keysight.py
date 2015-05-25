7c7005099e41611d3bc6f7292c685de58a89fc92
Alex Grede <agrede@gmail.com>
2015-05-25 12:54:47 -0400

    Added keysight and started on OSCOPE

---
+++ keysight.py
@@ -0,0 +1,95 @@
+import re
+from aopliab_common import within_limits
+import numpy as np
+
+
+class InfiniiVision5000():
+    """
+    PyVISA wrapper for InfiniiVision 5000 Oscilloscope
+    """
+
+    config = {}
+
+    def __init__(self, inst):
+        self.inst = inst
+        self.inst.write("*RST; *CLS")
+
+    @property
+    def acq_type(self):
+        tmp = self.inst.query("ACQ:TYPE?")
+        for idx, x in enumerate(self.config['acqtypes']):
+            if (re.match(x+".*", tmp)):
+                return idx
+
+    @acq_type.setter
+    def acq_type(self, value):
+        if within_limits(value, [0, 3]):
+            self.inst.write("ACQ:TYPE " + self.config['acqtypes'][int(value)])
+
+    @property
+    def acq_count(self):
+        return self.inst.query_ascii_values("ACQ:COUN?")[0]
+
+    @acq_count.setter
+    def acq_count(self, value):
+        if within_limits(value, [2, 65536]):
+            self.inst.write("ACQ:COUN %d" % value)
+
+    @property
+    def format(self):
+        tmp = self.inst.query("WAV:FORM?")
+        for idx, x in enumerate(self.config['formats']):
+            if (re.match(x+".*", tmp)):
+                return idx
+
+    @format.setter
+    def format(self, value):
+        if within_limits(value, [0, 2]):
+            self.inst.write("WAV:FORM " + self.config['formats'][int(value)])
+
+    @property
+    def points_mode(self):
+        tmp = self.inst.query("WAV:POIN:MODE?")
+        for idx, x in enumerate(self.config['points_modes']):
+            if (re.match(x+".*", tmp)):
+                return idx
+
+    @points_mode.setter
+    def points_mode(self, value):
+        if within_limits(value, [0, 2]):
+            self.inst.write("WAV:POIN:MODE " +
+                            self.config['points_modes'][int(value)])
+
+    @property
+    def points(self):
+        return int(self.query_ascii_values("WAV:POIN?")[0])
+
+    @points.setter
+    def points(self, value):
+        M = np.floor(np.log10(value))
+        m = value/10**M
+        v = self.points
+        if (M == 2 and m < 5 and m >= 2.5):
+            v = 250
+        elif ((M == 6 and m >= 8) or M > 6):
+            v = 8e6
+        elif (m >= 5):
+            v = m*10**M
+        elif (m >= 2):
+            v = m*10**M
+        else:
+            v = 10**M
+        self.inst.write("WAV:POIN %d" % v)
+
+    @property
+    def voltages(self):
+        pass
+
+    @property
+    def times(self):
+        pre = self.preamble
+        return (np.arange(pre[2])-pre[6])*pre[4]+pre[5]
+
+    @property
+    def preamble(self):
+        return self.inst.query_ascii_values("WAV:PRE?")
