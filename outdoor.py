2d276db611fb73aadde74db1e18e8a3f7744c323
Alex J. Grede <agrede@gmail.com>
2016-07-20 11:48:37 +0000

    Update 2400 and rewrite outdoor.py

--- outdoor.py
+++ outdoor.py
@@ -1,71 +1,70 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jul 18 09:14:07 2016
-
-@author: jspri
-"""
-
 import numpy as np
 import visa
 import aopliab_common as ac
 from keithley import K2400
 import time
+import matplotlib.pyplot as plt
 
 rm = visa.ResourceManager()
-smui = ac.getInstr(rm, "SMU")
-#pyri = ac.getInstr(rm, "LasPow")
+smui = ac.getInstr(rm, "LasPow")
+# pyri = ac.getInstr(rm, "LasPow")
 smu = K2400(smui)
-#pyr = K2400(pyri)
-#ard = ac.getInstr(rm, "ARD")
-vs = np.arange(-1., 3.01, 0.01)
-climit = 100e-3
+# pyr = K2400(pyri)
+ard = ac.getInstr(rm, "ARD")
+start = -0.2
+stop = 3.5
+points = 351
+climit = 50e-3
 pth = "./dta/outdoor%d.npz"
 
 dta = []
-k = 20
+k = 30
 
-def ivs(smu, vs, climit, port):
+
+def ivs(smu, start, stop, points, climit, port):
     smu.output = False
+    smu.source_volt = True
     smu.front_terminal = port
-    smu.set_voltage(vs[0])
     smu.current_limit = climit
-    ms = np.zeros((vs.size, 2))
-    smu.output = True
-    for k, v in enumerate(vs):
-        smu.set_voltage(v)
-        ms[k, :] = smu.measurement()[:2]
-    smu.output = False
-    return ms
-    
-def suns(smu, climit):
-    smu.output = False
-    smu.front_terminal = False
-    smu.set_voltage(0.)
-    smu.current_limit = climit
-    smu.output = True
-    ms = smu.measurement()[:2]
-    smu.output = False
-    return ms
-    
+    smu.voltage = start
+    tmp = smu.voltage_sweep(start, stop, points)
+    return np.reshape(tmp[1], (-1, 5))[:, :2]
+
+
+def suns(ard):
+    return ard.query_ascii_values("meas")[0]
+
+
 def align(smu, climit, ard):
     smu.output = False
-    smu.font_terminal = True
-    smu.set_voltage(0.)
+    smu.front_terminal = True
+    smu.voltage = 0.
     smu.current_limit = climit
     smu.output = True
-    #ard.write("start")
-    #time.sleep(10.)
-    input("Press Enter for next measurement...")
-    #ard.write("stop")
+    # ard.write("start")
+    while True:
+        user_input = input('Continue (y/n): ')
+        if user_input in ['y', 'n']:
+            break
+    # ard.write("stop")
+    # input("Press Enter When Stopped...")
     smu.output = False
-    
+    return (user_input == 'y')
 
-while(True):
-    align(smu, climit, None)
-    a = ivs(smu, vs, climit, True)
-    #b = ivs(smu, vs, climit, False)
-    c = suns(smu, climit)
+
+p = ac.DynamicPlot(ptype="semilogy")
+cont = True
+while(cont):
+    cont = align(smu, climit, ard)
+    if not cont:
+        break
+    a = ivs(smu, start, stop, points, climit, True)
+    b = ivs(smu, start, stop, points, climit, False)
+    # c = suns(smu, climit)
     tme = time.localtime()
-    dta.append((tme, a, c))
+    x = tme.tm_hour+tme.tm_min/60.+tme.tm_sec/3600.
+    y = (-a[:, 0]*a[:, 1]).max()/(-b[:, 0]*b[:, 1]).max()
+    p.update(x, y)
+    dta.append((tme, a, b))
     np.savez_compressed(pth % k, dta)
     k = k+1
